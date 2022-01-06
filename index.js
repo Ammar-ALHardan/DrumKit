4f572bd50d2ecdb2702c4d85e14857a271a1d3fd
Ammar-ALHardan <ammarhardan863@gmail.com>
2022-01-06 14:42:13 +0200

    Add A step 1 of Java Sript

---
+++ index.js
@@ -0,0 +1,14 @@
+// var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+// for (var i = 0 ; i<numberOfDrumButtons ; i++){
+//     document.querySelectorAll(".drum")[i].addEventListener("click", function hand(){
+//         alert('ammar');
+//     });
+    
+// }
+
+var numberOfDrumButton = document.querySelectorAll(".drum").length;
+for(var i = 0 ; i<numberOfDrumButton ; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        alert('ammar');
+    });
+} 
